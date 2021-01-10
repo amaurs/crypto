a7eee247645ebe503ee538875866449f94795703
amaurs <amaury.gtz@gmail.com>
2021-01-09 17:49:12 -0800

    ⚖️ Implementing simple balancing strategy.

--- handler.py
+++ handler.py
@@ -42,22 +42,11 @@ class BitsoClient:
             "Authorization": self._build_auth_header(http_method="GET", request_path=request_path)})
         return response.json()
 
-    def put_order(self):
+    def put_order(self, parameters: Dict) -> Dict:
         request_path = "/v3/orders/"
-
-        parameters = {'book': 'xrp_mxn',
-                      'type': 'market',
-                      'side': 'buy',
-                      'minor': 100}
-
         response = requests.post(f"https://api.bitso.com{request_path}", json=parameters, headers={
             "Authorization": self._build_auth_header(http_method="POST", request_path=request_path,
                                                      parameters=parameters)})
-        return response.json()
-
-    def get_books(self) -> Dict:
-        request_path = "/v3/available_books/"
-        response = requests.get("https://api.bitso.com" + request_path)
         return response.json()
 
     def get_books(self) -> Dict:
@@ -74,13 +63,54 @@ def handler(event: Dict, context: Dict) -> Dict:
 
     bitso_client = BitsoClient(bitso_key=API_KEY, bitso_secret=API_SECRET)
     balances = bitso_client.get_balace()
-    interest = filter(lambda x: x.get("currency") in TARGET_BALANCE.keys(), balances.get("payload").get("balances"))
-    print(json.dumps(list(interest), indent=4))
+    interests = filter(lambda x: x.get("currency") in TARGET_BALANCE.keys(), balances.get("payload").get("balances"))
+    pivot = "mxn"
+
+    portfolio_value = 0.0
+    current_allocation = {}
+    for interest in interests:
+        currency = interest.get("currency")
+        if currency != pivot:
+            ticker = bitso_client.get_ticker(f"{currency}_{pivot}")
+            latest_price = float(ticker.get("payload").get("last"))
+            currency_value = float(interest.get("available")) * latest_price
+        else:
+            currency_value = float(interest.get("available"))
+        current_allocation.update({currency: currency_value})
+        portfolio_value += currency_value
+
+    print(f"total: {portfolio_value}")
+
+    ideal_allocation = {currency: portfolio_value * data.get("allocation") for currency, data in TARGET_BALANCE.items()}
+
+    orders = []
+    for currency, current in current_allocation.items():
+        diff = ideal_allocation.get(currency) - current
+        orders.append((currency, diff))
+
+    for currency, quantity in orders:
+        if currency == pivot:
+            continue
+
+        if quantity < 0:
+            parameters = {'book': f'{currency}_{pivot}',
+                          'type': 'market',
+                          'side': 'sell',
+                          'minor': (-1) * quantity}
+
+        else:
+            parameters = {'book': f'{currency}_{pivot}',
+                          'type': 'market',
+                          'side': 'buy',
+                          'minor': quantity}
+        print(json.dumps(parameters, indent=4))
+
     return {"status": 200}
 
 
 if __name__ == '__main__':
-    print(json.dumps(TARGET_BALANCE))
+
+    print(json.dumps(TARGET_BALANCE, indent=4))
     handler({}, {})
 
 
