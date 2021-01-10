2e3d466beb1a529aa678d3c9f8fc092949681ce8
amaurs <amaury.gtz@gmail.com>
2021-01-09 16:16:19 -0800

    👾 Adding a class to make calls to the bitso API.

--- handler.py
+++ handler.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+from dataclasses import dataclass
 from typing import Dict
 import hmac
 import hashlib
@@ -9,27 +10,77 @@ import requests
 
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
+TARGET_BALANCE = json.loads(os.getenv("TARGET_BALANCE"))
+
+@dataclass
+class BitsoClient:
+    bitso_key: str
+    bitso_secret: str
+
+    @staticmethod
+    def _build_nonce() -> str:
+        return str(int(round(time.time() * 1000)))
+
+    def _build_auth_header(self, http_method: str, request_path: str, parameters: Dict[str, str] = None):
+        nonce = self._build_nonce()
+        message = self._build_message(http_method=http_method, request_path=request_path, nonce=nonce, parameters=parameters)
+        signature = hmac.new(self.bitso_secret.encode('utf-8'),
+                             message.encode('utf-8'),
+                             hashlib.sha256).hexdigest()
+        return f"Bitso {self.bitso_key}:{nonce}:{signature}"
+
+    def _build_message(self, http_method: str, request_path: str, nonce:str, parameters: Dict[str, str]) -> str:
+        parameters = parameters or {}
+
+        if http_method == "POST" and parameters:
+            return f"{nonce}{http_method}{request_path}{json.dumps(parameters)}"
+        return f"{nonce}{http_method}{request_path}"
+
+    def get_balace(self):
+        request_path = "/v3/balance/"
+        response = requests.get(f"https://api.bitso.com{request_path}", headers={
+            "Authorization": self._build_auth_header(http_method="GET", request_path=request_path)})
+        return response.json()
+
+    def put_order(self):
+        request_path = "/v3/orders/"
+
+        parameters = {'book': 'xrp_mxn',
+                      'type': 'market',
+                      'side': 'buy',
+                      'minor': 100}
+
+        response = requests.post(f"https://api.bitso.com{request_path}", json=parameters, headers={
+            "Authorization": self._build_auth_header(http_method="POST", request_path=request_path,
+                                                     parameters=parameters)})
+        return response.json()
+
+    def get_books(self) -> Dict:
+        request_path = "/v3/available_books/"
+        response = requests.get("https://api.bitso.com" + request_path)
+        return response.json()
+
+    def get_books(self) -> Dict:
+        request_path = "/v3/available_books/"
+        response = requests.get("https://api.bitso.com" + request_path)
+        return response.json()
+
+    def get_ticker(self, ticker) -> Dict:
+        request_path = f"/v3/ticker/?book={ticker}"
+        response = requests.get("https://api.bitso.com" + request_path)
+        return response.json()
 
 def handler(event: Dict, context: Dict) -> Dict:
-    request_path = "/v3/orders/"
-    nonce = str(int(round(time.time() * 1000)))
-    parameters = {'book': 'xrp_mxn',
-                  'type': 'market',
-                  'side': 'buy',
-                  'minor': 1568.57}
 
-    message = nonce + "POST" + request_path + json.dumps(parameters)
+    bitso_client = BitsoClient(bitso_key=API_KEY, bitso_secret=API_SECRET)
+    balances = bitso_client.get_balace()
+    interest = filter(lambda x: x.get("currency") in TARGET_BALANCE.keys(), balances.get("payload").get("balances"))
+    print(json.dumps(list(interest), indent=4))
+    return {"status": 200}
 
-    signature = hmac.new(API_SECRET.encode('utf-8'),
-                         message.encode('utf-8'),
-                         hashlib.sha256).hexdigest()
-
-    auth_header = f'Bitso {API_KEY}:{nonce}:{signature}'
-
-    response = requests.post("https://api.bitso.com" + request_path, json=parameters,
-                             headers={"Authorization": auth_header})
-
-    return response
 
 if __name__ == '__main__':
-    print(json.dumps(handler({}, {}), indent=4))
+    print(json.dumps(TARGET_BALANCE))
+    handler({}, {})
+
+
